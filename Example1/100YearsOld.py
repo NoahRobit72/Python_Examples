8b5dd0a09cf4eb411510d09df479dc3ed52ef356
Noah Robitshek <64294283+NoahRobit72@users.noreply.github.com>
2023-01-27 09:27:30 -0500

    add 100 years old problem

---
+++ Example1/100YearsOld.py
@@ -0,0 +1,7 @@
+# This program is to print the year that the user will be 100 years old
+
+age = input("How old are you: ")
+yearBorn = 2023 - int(age)
+year100 = yearBorn + 100
+print('You will be 100 in year: ', year100)
+
